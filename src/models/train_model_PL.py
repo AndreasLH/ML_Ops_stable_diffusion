d7dbd8154a5d1301db2e2fbb0c4338316de7801e
Malthe Andersen <s194257@student.dtu.dk>
2023-01-06 15:30:26 +0100

    Config files works now
    It has been tested

--- src/models/train_model_PL.py
+++ src/models/train_model_PL.py
@@ -1,11 +1,14 @@
+import os
+
 import pytorch_lightning as pl
 from src.models.model import UNet2DModelPL
 from torch.utils.data import Dataset, DataLoader
 from src.data.dataloader import ButterflyDataloader
+import hydra
 
 import torch
 
-@hydra.main(config_path="conf", config_name="config.yaml")
+@hydra.main(config_path="../../conf/", config_name="config.yaml")
 def main(cfg):
     hpms = cfg.experiment['hyperparameters']
     seed = hpms.seed
@@ -19,7 +22,7 @@ def main(cfg):
     torch.manual_seed(seed) #Set seed
 
 
-    path = "C:/Users/elleh/OneDrive/MachineLearningOperation/project/ML_Ops_stable_diffusion/data/processed/train.pt"
+    path = os.path.join(os.getcwd(),"data/processed/train.pt")
 
     model = UNet2DModelPL(sample_size,learning_rate)
     trainer = pl.Trainer(max_epochs=epochs, log_every_n_steps=log_frequency)
