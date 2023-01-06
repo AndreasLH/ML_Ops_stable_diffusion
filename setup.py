4be3215acadf07ead02e11ad79e2c2f096fbe48d
Andreas Lau Hansen <s194235@student.dtu.dk>
2023-01-06 10:20:36 +0100

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A project for the DTU course Machine Learning Operations. The primary focus is on MLOps and not the models specifically',
+    author='Group Awesome 21',
+    license='MIT',
+)
