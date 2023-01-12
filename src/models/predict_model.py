3c02560ced40561223555062eefc6dcae703104d
Christian <s194255@student.dtu.dk>
2023-01-12 14:24:30 +0100

    første udkast

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,49 +1,25 @@
-import logging
+from src.models.model import UNet2DModelPL
+from src import _PROJECT_ROOT
 import os
-
 import hydra
-import numpy as np
-import torch
-from diffusers import DDPMPipeline, DDPMScheduler
-from hydra.utils import get_original_cwd
-from omegaconf import OmegaConf
 from PIL import Image
 
-import wandb
-from src.data.dataset import ButterflyDataset
-from src.models.model import UNet2DModelPL
 
-noise_scheduler = DDPMScheduler(num_train_timesteps=1000)
+@hydra.main(version_base=None, config_path=os.path.join(_PROJECT_ROOT, 'conf'), config_name="config.yaml")
+def evaluate(conf):
+    hpms = conf.experiment['hyperparameters']
+    path = os.path.join(_PROJECT_ROOT, 'outputs/2023-01-12/14-20-40/mlopsproject21/2fpleg5r/checkpoints/epoch=0-step=1.ckpt')
+    model = UNet2DModelPL.load_from_checkpoint(path, sample_size=hpms.image_size)
 
-log = logging.getLogger(__name__)
-# wandb.init(project='Butterflies')
-
-# python src/models/predict_model.py hydra.job.chdir=True
-
-@hydra.main(version_base=None, config_path="../../conf", config_name="config.yaml")
-def evaluate(config):
-    # Sample some images from random noise (this is the backward diffusion process).
-    # The default pipeline output type is `List[PIL.Image]`
-    log.info(f"configuration: \n{OmegaConf.to_yaml(config)}")
-    config = config.experiment
-    model_checkpoint =  get_original_cwd()+"/models/model.ckpt"
-    model = UNet2DModelPL()
-    # state_dict = torch.load(model_checkpoint, map_location=torch.device('cpu'))
-    # model.load_state_dict(state_dict)
-# C:\Users\andre\OneDriveDTU\MLops 02476\ML_Ops_stable_diffusion\models\lightning_logs\version_0\checkpoints\epoch=2-step=30.ckpt
-    model = UNet2DModelPL.load_from_checkpoint(model_checkpoint)
-    model.eval()
-
-    images = model.sample(config.hparams.eval_batch_size, config.hparams.seed)
+    images = model.sample(hpms.eval_batch_size)
 
     # Make a grid out of the images
     image_grid = make_grid(images, rows=4, cols=4)
 
     # Save the images
-    test_dir = os.path.join(config.hparams.output_dir, "samples")
+    test_dir = os.path.join(_PROJECT_ROOT, "samples")
     os.makedirs(test_dir, exist_ok=True)
-    image_grid.save(f"{test_dir}/hej.png")
-
+    image_grid.save(f"{test_dir}/output.png")
 
 def make_grid(images, rows, cols):
     w, h = images[0].size
@@ -52,5 +28,4 @@ def make_grid(images, rows, cols):
         grid.paste(image, box=(i%cols*w, i//cols*h))
     return grid
 
-if __name__ == "__main__":
-    evaluate()
+evaluate()
