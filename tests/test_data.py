1babecc6b81e93b3d1ba73af26e7c8bb89712755
TECH-yufu <yuch0001@gmail.com>
2023-01-12 12:13:44 +0100

    Update test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -16,3 +16,13 @@ def test_data_length():
 def test_data_shape():
     dataset = ButterflyDataset(os.path.join(_PATH_DATA, 'processed/train.pt'))
     assert torch.all(torch.tensor([i['images'].shape == (3, 128, 128) for i in dataset])) == True, "Samples in the dataset does not have the correct shape (3, 128, 128)" # assert all images have shape (3, 128, 128) (channel, width, height)
+
+@pytest.mark.skipif(not os.path.exists(f'{_PATH_DATA}/processed'), reason="Data files not found") # skip this test if dir data/processed does not exist
+def test_dataloader():
+    dataset = ButterflyDataset(os.path.join(_PATH_DATA, 'processed/train.pt'))
+    dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
+
+    batch = next(iter(dataloader))['images']
+    assert batch.shape == (64, 3, 128, 128), "Batch does not have the expected shape (64, 3, 128, 128)"
+    assert batch.dtype == torch.float32, "Batch does not have expected dtype, torch.float32"
+
