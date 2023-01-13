d4d756c00d6339afcca58ba991f18276bbdc1074
Malthe Andersen <s194257@student.dtu.dk>
2023-01-13 16:00:54 +0100

    Update test_train.py

--- tests/test_train.py
+++ tests/test_train.py
@@ -26,10 +26,10 @@ def test_training_loop_PL():
     assert loss.dtype == torch.float32
 
 
-#@pytest.mark.skipif(not os.path.exists(f'{_PATH_DATA}/processed'), reason="Data files not found")  # skip this test if dir data/processed does not exist
-#def test_evaluation_PL():
-#    dataset = ButterflyDataset(os.path.join(_PATH_DATA, 'processed/train.pt'))
-#    dataloader = DataLoader(dataset, batch_size=8, shuffle=True)
-#    batch = next(iter(dataloader))
-#    Inception_score = model.validation_step(batch, batch_idx=1)
-#    assert Inception_score.dtype == torch.float32
+@pytest.mark.skipif(not os.path.exists(f'{_PATH_DATA}/processed'), reason="Data files not found")  # skip this test if dir data/processed does not exist
+def test_evaluation_PL():
+    dataset = ButterflyDataset(os.path.join(_PATH_DATA, 'processed/train.pt'))
+    dataloader = DataLoader(dataset, batch_size=8, shuffle=True)
+    batch = next(iter(dataloader))
+    Inception_score = model.validation_step(batch, batch_idx=1)
+    assert Inception_score.dtype == torch.float32
