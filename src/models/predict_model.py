331e908a0a00637829ffb8ccdc6ec8cb409f6c81
Christian <s194255@student.dtu.dk>
2023-01-13 11:14:45 +0100

    Update predict_model.py

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,6 +1,7 @@
 import os
 
 import click
+import numpy as np
 import yaml
 from PIL import Image
 
@@ -18,20 +19,25 @@ def evaluate(model_dir):
     hpms = conf["experiment"]["hyperparameters"]
 
     for checkpoint in os.listdir(os.path.join(_PROJECT_ROOT, model_dir, "checkpoints")):
+        n = hpms["eval_batch_size"]
+        root = int(np.sqrt(n))
+        assert root**2 == n, "eval_batch_size must be quadratic"
+        rows, cols = root, root
+
         model_path = os.path.join(_PROJECT_ROOT, model_dir, "checkpoints", checkpoint)
         model = UNet2DModelPL.load_from_checkpoint(
             model_path, sample_size=hpms["image_size"]
         )
-        images = model.sample(hpms["eval_batch_size"])
-        image_grid = make_grid(images, rows=4, cols=4)
+        images = model.sample(n)
+        image_grid = make_grid(images, rows, cols)
         test_dir = os.path.join(_PROJECT_ROOT, "samples")
         os.makedirs(test_dir, exist_ok=True)
         image_grid.save(f"{test_dir}/{os.path.basename(model_path)[:-5]}.png")
 
 
-# todo: make_grid skal automatisk vælge rows og cols
 def make_grid(images, rows, cols):
     w, h = images[0].size
+
     grid = Image.new("RGB", size=(cols * w, rows * h))
     for i, image in enumerate(images):
         grid.paste(image, box=(i % cols * w, i // cols * h))
